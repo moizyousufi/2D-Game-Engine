c960ebb064c5fac0f393b5e5adc2e3ba7b1f7367
Moiz Yousufi <moiz.yousufi@outlook.com>
2024-02-26 13:48:28 -0600

    changed from g++ to cmake

---
+++ build/CMakeFiles/Game.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Game.dir/game.cpp.o"
+  "CMakeFiles/Game.dir/game.cpp.o.d"
+  "Game"
+  "Game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
